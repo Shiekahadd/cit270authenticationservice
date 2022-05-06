7e674e6224bc93dd050fad8eb7a4eaa490d31601
Shiekahadd <ali19003@byui.edu>
2022-05-06 13:44:12 -0600

    friday may 6, 2022 class

--- apiclient.bat
+++ apiclient.bat
@@ -1,4 +1,6 @@
-# curl command below
+rem curl command below
 
-curl -d "@data.json" -X POST http://localhost:3000/login
+curl -d "@data.json" -X POST -H "Content-Type:application/json" http://localhost:3000/login
+
+
 
--- server.js
+++ server.js
@@ -15,8 +15,9 @@ app.post('/login',(request,response)=>{//a post is when a client sends new infor
 const loginRequest = request.body;
 console.log("Request Body", JSON.stringify(request.body));
 
-if (loginRequest.userName=="sali@gmail.com" && loginRequest.password=="Dist1#"){
+if (loginRequest.userName=="sali@gmail.com" && loginRequest.password=="Digt1#"){
     response.status(200); //200 means OK
+    response.send("Welcome");
 } else{ 
     response.status(401); //401 means unauthorized
     response.send("Unauthorized");
@@ -24,6 +25,7 @@ if (loginRequest.userName=="sali@gmail.com" && loginRequest.password=="Dist1#"){
 });
 
 app.get('/',(request,response)=>{//every time something calls your API that is a request
+
     response.send("Hello");}) //a response is when the API gives the information requested
 
 
