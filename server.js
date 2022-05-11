8b1aa4c74ef0b90e0cc738db73c47669a8d1d85b
Shiekahadd <ali19003@byui.edu>
2022-05-11 13:46:18 -0600

    wednesday 11 may 2022 class

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const bodyParser = require('body-parser'); //body parser is called middleware
 const port = 3000;
 const app = express(); //import the library
-
+const md5 = require('md5');
 app.use(bodyParser.json()); //use the middleware (call it before anything else happens on each request)
 
 app.listen(port, ()=>{
@@ -14,6 +14,9 @@ app.listen(port, ()=>{
 app.post('/login',(request,response)=>{//a post is when a client sends new information to an API
 const loginRequest = request.body;
 console.log("Request Body", JSON.stringify(request.body));
+//search database for username, and retrieve current password
+
+//compare the hashed version of the password that was sent with the hashed version from the database
 
 if (loginRequest.userName=="sali@gmail.com" && loginRequest.password=="Digt1#"){
     response.status(200); //200 means OK
