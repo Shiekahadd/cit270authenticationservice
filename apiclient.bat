b6184d92cff219541bcb4938d9a7e21353c59e07
Shiekahadd <ali19003@byui.edu>
2022-06-06 13:41:04 -0600

    monday 6 june 2022 class

--- apiclient.bat
+++ apiclient.bat
@@ -1,6 +1,6 @@
 rem curl command below
 
-curl --insecure  -d "@data.json" -X POST -H "Content-Type:application/json" http://localhost:3000/login
+curl  -d "@data.json" -X POST -H "Content-Type:application/json" http://localhost:3000/login
 
 
 
