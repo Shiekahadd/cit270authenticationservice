09c700e1e33f90d049f487494deea7bbf02f35a9
Shiekahadd <ali19003@byui.edu>
2022-06-17 13:59:39 -0600

    fridat 17 june 2022 class

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const bodyParser = require('body-parser'); //body parser is called middleware. R
 const {createClient} = require('redis'); //This is called destructuring.
 const { fstat } = require('fs');
 
-const client = createClient({ url: 'redis://default:@10.128.0.2:6379', }); //This 
+const redisClient = createClient({ url: 'redis://default:@10.128.0.2:6379', }); //This makes a connection to redis database
 
 
 
